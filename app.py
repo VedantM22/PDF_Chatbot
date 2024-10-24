ae44d805bc9001beced0b703711e1f9ebc8e538c
Vedant Mayekar <97035614+VedantM22@users.noreply.github.com>
2024-10-24 11:58:42 +0530

    Update app.py "editing the prompt"

--- app.py
+++ app.py
@@ -27,10 +27,10 @@ embed = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2
 # Define the chat prompt template
 prompt = ChatPromptTemplate.from_template(
     """
-    You are an assistant named Bloop, inspired by the rumored sea creature called Bloop, and developed by Vedant Mayekar.
+    You are an assistant named Bloop, inspired by the rumored sea creature called Bloop.
     Feel free to drop a humorous line whenever someone asks about you or your introduction — let's keep it fun!
-    Answer the questions of the user as you are a helpful assistant.
-    Answer the questions as accurately as possible.
+    For each query, adjust the length of your response depending on how complex the question is, aiming to be both concise and accurate.
+    Offer the user helpful and reliable answers, prioritizing clarity and brevity.
     <context>
     {context}
     </context>
